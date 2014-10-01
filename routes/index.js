1cc4d54e27b0190e8b23ca87d60374a05b4a263f
Guillermo Vargas <gjvargas@mit.edu>
2014-10-01 11:10:54 -0400

    basic posting is functional

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  var db = req.db;
+  var posts = db.get('posts');
+  posts.find({}, function(e, docs){
+    res.render('index', { title: 'Express', 'posts': docs });
+  });
 });
+
+router.get('/new', function(req, res) {
+	res.render('new', { title: 'Make a new Post' });
+});
+
+router.post('/post', function(req, res, next) {
+	var db = req.db;
+  var posts = db.get('posts');
+	posts.insert({"title": req.body.title, "post": req.body.post}, function(err, docs){
+	if(err) {
+		res.send("There was a problem");
+		} else {
+			res.redirect("/");
+		}
+	});
+});
+
 
 module.exports = router;
