0ae0277ed775a2bb59acc9818627579505ad3d45
Guillermo Vargas <gjvargas@mit.edu>
2014-10-04 21:17:53 -0400

    I think it woks

--- app.js
+++ app.js
@@ -25,6 +25,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cookieSession({
+  key: 'app.sess',
+  secret: 'SUPERsekret',
+  user: null
+}));
+
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,16 @@ var cookieSession = require('cookie-session');
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  req.session.user = null;
   res.render('index', {title: 'Fritter'});
 });
 
 router.get('/new', function(req, res) {
 	res.render('new', { title: 'Make a new Post' });
+});
+
+router.get('/signout', function (req, res, next) {
+  res.redirect('/');
 });
 
 router.post('/login', function (req, res, next) {
@@ -23,9 +28,7 @@ router.post('/login', function (req, res, next) {
     } else if(req.body.pass != docs[0].password) {
       res.send("Incorrect password.");
     } else {
-
-      res.cookie('user', req.body.user, {maxAge:2678400000});
-      res.cookie('pass', req.body.pass, {maxAge:2678400000});
+      req.session.user = req.body.user;
       res.redirect("/posts");
     }
   });
@@ -51,6 +54,7 @@ router.post('/create', function(req, res, next) {
       if(e) {
         res.send("There was a problem");
         } else {
+          req.session.user = req.body.user;
           res.redirect("/posts");
         }
       });
@@ -64,7 +68,7 @@ router.get('/posts', function(req, res) {
   var db = req.db;
   var posts = db.get('posts');
   posts.find({}, function(e, docs){
-    res.render('posts', {title: "Fritter", 'posts': docs, 'cookies': req.cookies});
+    res.render('posts', {title: "Fritter", 'posts': docs, 'user': req.session.user});
   });
 });
 
@@ -73,7 +77,7 @@ router.post('/post', function(req, res, next) {
 	var db = req.db;
   var posts = db.get('posts');
   if(req.body.title.length > 0 && req.body.post.length > 0) {
-  	posts.insert({"title": req.body.title, "post": req.body.post, "user": req.cookies.user}, function(err, docs){
+  	posts.insert({"title": req.body.title, "post": req.body.post, "user": req.session.user}, function(err, docs){
   	if(err) {
   		res.send("There was a problem");
   		} else {
@@ -81,6 +85,37 @@ router.post('/post', function(req, res, next) {
   		}
   	});
   }
+});
+
+router.post('/edit', function(req, res, next) {
+  var db = req.db;
+  var posts = db.get('posts');
+  posts.remove({"title": req.body.orig_title, "post": req.body.orig_post, "user": req.session.user}, function(err, docs){
+  if(err) {
+    res.send("There was a problem");
+    }
+  });
+  if(req.body.title.length > 0 && req.body.post.length > 0) {
+    posts.insert({"title": req.body.title, "post": req.body.post, "user": req.session.user}, function(err, docs){
+    if(err) {
+      res.send("There was a problem");
+      } else {
+        res.redirect("/posts");
+      }
+    });
+  }
+});
+
+router.post('/delete', function(req, res, next) {
+  var db = req.db;
+  var posts = db.get('posts');
+  posts.remove({"title": req.body.title, "post": req.body.post, "user": req.session.user}, function(err, docs){
+  if(err) {
+    res.send("There was a problem");
+    } else {
+      res.redirect("/posts");
+    }
+  });
 });
 
 
